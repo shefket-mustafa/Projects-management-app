9897baac6a786aad169473632b4fc2aa54505387
shefket-mustafa <miracle_1@abv.bg>
2025-04-30 00:55:30 +0300

    src/App.jsx
    Add project comp added and handlers for it created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react"
+import LeftContainer from "./components/left-container/LeftContainer"
 import Main from "./components/main/Main"
+import AddProject from "./components/add-project/AddProject";
 
 
 function App() {
+  const [isAddProjClicked, setIsAddProjClicked] = useState(false);
+  const [projects, setProjects] = useState([]);
+
+  const addProjHandler = () => {
+    setIsAddProjClicked(state => !state);
+  }
+
+  const addProjectHandler = (project) => {
+
+    setProjects(prevProjects => [...prevProjects, project]);
+  }
 
   return (
     <>
-      <Main />
+    <div className="main-container">
+
+    <LeftContainer 
+    addHandler={addProjHandler} 
+    projects={projects}
+    
+    />
+
+      {isAddProjClicked ? <AddProject addProject={addProjectHandler}/> : <Main />}
+      
+
+      </div>
     </>
   )
 }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,16 +3,7 @@ import './main.css';
 
 export default function Main() {
   return (
-      <>
-      <div className="main-container">
-        
-      <div className="left-container">
-      <h1>YOUR PROJECTS</h1>
-      <div className='add-proj'>
-      <button>+ Add project</button>
-      </div>
-      </div>
-
+      
       <div className="home-container">
         <img src={image} alt="" />
         <p style={{fontFamily: 'sans-serif', fontWeight: 'bold', color: 'grey', marginBottom:'0', marginTop:'10px'}}>No Project Selected</p>
@@ -20,7 +11,5 @@ export default function Main() {
         <button style={{backgroundColor: 'rgb(51, 26, 10)', fontSize:'16px', color: 'grey', padding: '12px 20px', borderRadius: '5px', border: 'none'}}>Create new project</button>
       </div>
 
-      </div>
-      </>
     );
 }
