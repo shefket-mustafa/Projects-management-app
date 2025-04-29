f54cac946480031fdbf50ece2e8b4ff766d4fcb3
shefket-mustafa <miracle_1@abv.bg>
2025-04-30 01:32:49 +0300

    architecture of comp made

--- src/components/saved-project/SavedProject.jsx
+++ src/components/saved-project/SavedProject.jsx
@@ -1,7 +1,26 @@
-export default function SavedProject() {
+import './savedProject.css'
+
+export default function SavedProject({project}) {
   return (
         <>
-        
+        <div className="details-container">
+
+          <div className="upper-container">
+          <h1>{project.title}</h1>
+          <button>Delete</button>
+          <p>{project.date}</p>
+          <p>{project.description}</p>
+          </div>
+
+          <div className="lower-container">
+            <h1>Tasks</h1>
+            <input type="text" />
+            <button>Add Task</button>
+
+            <div className="tasks"></div>
+          </div>
+
+        </div>
         </>     
     );
 }
