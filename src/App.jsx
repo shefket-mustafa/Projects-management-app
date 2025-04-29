46bea333e5a6f878ceac6b3cad3f7673e49b8674
shefket-mustafa <miracle_1@abv.bg>
2025-04-30 01:31:30 +0300

    added props to details comp and isAddProj set to false when clicking on an existing project

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import { useState } from "react"
 import LeftContainer from "./components/left-container/LeftContainer"
 import Main from "./components/main/Main"
 import AddProject from "./components/add-project/AddProject";
+import SavedProject from "./components/saved-project/SavedProject";
 
 
 function App() {
   const [isAddProjClicked, setIsAddProjClicked] = useState(false);
   const [projects, setProjects] = useState([]);
+  const [currentProject, setCurrentProject] = useState(null);
 
   const addProjHandler = () => {
     setIsAddProjClicked(state => !state);
   }
 
   const addProjectHandler = (project) => {
-
     setProjects(prevProjects => [...prevProjects, project]);
   }
+
+  const projectHandler = (project) => {
+    setCurrentProject(project);
+    setIsAddProjClicked(false);
+  };
 
   return (
     <>
@@ -24,10 +30,11 @@ function App() {
     <LeftContainer 
     addHandler={addProjHandler} 
     projects={projects}
+    projectHandler={projectHandler}
     
     />
 
-      {isAddProjClicked ? <AddProject addProject={addProjectHandler}/> : <Main />}
+      {isAddProjClicked ? (<AddProject addProject={addProjectHandler}/>) : currentProject ? (<SavedProject project={currentProject}/>) : <Main/>}
       
 
       </div>
