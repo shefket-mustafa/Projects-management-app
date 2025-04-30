270751750238918b1f3b456ab8020c5a37012709
shefket-mustafa <miracle_1@abv.bg>
2025-04-30 17:53:06 +0300

    Changes made to the comp
    uuid used for id of the list items(tasks), a handler for adding a task created(it will add the task to the current project, which is also updated when we add a task as we rely upon it for the details page(savedProject))

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LeftContainer from "./components/left-container/LeftContainer"
 import Main from "./components/main/Main"
 import AddProject from "./components/add-project/AddProject";
 import SavedProject from "./components/saved-project/SavedProject";
+import { v4 as uuid} from 'uuid';
 
 
 function App() {
@@ -10,18 +11,47 @@ function App() {
   const [projects, setProjects] = useState([]);
   const [currentProject, setCurrentProject] = useState(null);
 
+
   const addProjHandler = () => {
     setIsAddProjClicked(state => !state);
   }
 
   const addProjectHandler = (project) => {
-    setProjects(prevProjects => [...prevProjects, project]);
+    const newProject = {
+      ...project,
+      tasks:[],
+      id: uuid()
+    };
+    setProjects(prevProjects => [...prevProjects, newProject]);
   }
 
   const projectHandler = (project) => {
     setCurrentProject(project);
     setIsAddProjClicked(false);
   };
+
+  const addTask = (projectId, taskTitle) => {
+    setProjects(prevProjects => {
+
+      const updated =  prevProjects.map(project => project.id === projectId ? {
+      ...project,
+      tasks:[
+        ...project.tasks,
+        {id: uuid(),
+          title: taskTitle
+        }
+      ]
+    } : project)
+  
+
+    const updatedCurrent = updated.find(project => project.id === projectId)
+    setCurrentProject(updatedCurrent)
+
+    return updated
+  });
+
+
+  }
 
   return (
     <>
@@ -34,7 +64,12 @@ function App() {
     
     />
 
-      {isAddProjClicked ? (<AddProject addProject={addProjectHandler}/>) : currentProject ? (<SavedProject project={currentProject}/>) : <Main/>}
+      {isAddProjClicked 
+      ? (<AddProject addProject={addProjectHandler}/>) 
+      : currentProject 
+      ? (<SavedProject project={currentProject} addTask={addTask}/>) 
+      : <Main addHandler={addProjHandler}
+      />}
       
 
       </div>
