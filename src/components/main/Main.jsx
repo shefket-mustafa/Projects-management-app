c01528026edd935b5992e0c054b48e61c7fbf46b
shefket-mustafa <miracle_1@abv.bg>
2025-04-30 17:49:21 +0300

    functionality added to the create new project button

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,14 +1,14 @@
 import image from '/images/logo.png'
 import './main.css';
 
-export default function Main() {
+export default function Main({addHandler}) {
   return (
       
       <div className="home-container">
         <img src={image} alt="" />
         <p style={{fontFamily: 'sans-serif', fontWeight: 'bold', color: 'grey', marginBottom:'0', marginTop:'10px'}}>No Project Selected</p>
         <p style={{fontFamily: 'sans-serif',color:'gray', marginTop:'0px'}}>Select a project or get started with a new one</p>
-        <button style={{backgroundColor: 'rgb(51, 26, 10)', fontSize:'16px', color: 'grey', padding: '12px 20px', borderRadius: '5px', border: 'none'}}>Create new project</button>
+        <button onClick={addHandler}>Create new project</button>
       </div>
 
     );
