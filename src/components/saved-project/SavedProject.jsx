4b64a6a65d8abdc71a50c19fc41d4e1441611e41
shefket-mustafa <miracle_1@abv.bg>
2025-04-30 15:06:03 +0300

    date changed made to appear as in the shown project

--- src/components/saved-project/SavedProject.jsx
+++ src/components/saved-project/SavedProject.jsx
@@ -1,26 +1,43 @@
-import './savedProject.css'
+import { useState } from "react";
+import "./savedProject.css";
 
-export default function SavedProject({project}) {
+export default function SavedProject({ project }) {
+
+  const[savedTasks, setSavedTasks] = useState([]);
+
+  const addTaskHandler = (task) => {
+    setSavedTasks(prevTasks => [...prevTasks, task])
+  };
+
+
   return (
-        <>
-        <div className="details-container">
-
-          <div className="upper-container">
-          <h1>{project.title}</h1>
-          <button>Delete</button>
-          <p>{project.date}</p>
-          <p>{project.description}</p>
+    <>
+      <div className="details-container">
+        <div className="upper-container">
+          <div className="upper-header">
+            <h1>{project.title}</h1>
+            <button>Delete</button>
           </div>
+          <p style={{ color: "grey" }}>
+            {new Date(project.date).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}
+          </p>
+          <p>{project.description}</p>
+        </div>
 
-          <div className="lower-container">
-            <h1>Tasks</h1>
+        <div className="lower-container">
+          <h1>Tasks</h1>
+          <div className="tasks">
             <input type="text" />
             <button>Add Task</button>
-
-            <div className="tasks"></div>
           </div>
 
+          <div className="tasks"></div>
         </div>
-        </>     
-    );
+      </div>
+    </>
+  );
 }
